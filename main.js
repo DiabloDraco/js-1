a3aef2969d3d090603425244ce82e7ef254ea035
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-07-03 13:04:57 +0500

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var userName = prompt ("Your first name" , "Farrukh");
+var userSurname = prompt ("Your surname" , "Abbosov");
+var userAge = prompt ("Your age" , "65");
+var userHobby = prompt ("Your hobby", "Playing football");
+var userBirth = prompt ("Your birth date" , "24 march");
+var userPlace = prompt ("Where do you live" , "Tashkent");
+var userDream = prompt ("What's your dream", "accamulate 1mln$");
+var userBudget = prompt ("How money did you have", "1bln$");
+var userScore = prompt ("How did you rate this app", "5star");
+var userNajot = prompt ("How did you rate Najot ta'lim", "3star");
+
+alert (`User name: ${userName}\nUser Surname: ${userSurname}\nUser Age: ${userAge}\nUser Hobby: ${userHobby}\nUser Birth: ${userBirth}\nUser Place: ${userPlace}\nUser Dream: ${userDream}\nUser Budget: ${userBudget}\nUser Score: ${userScore}\nNajot score: ${userNajot}`);
